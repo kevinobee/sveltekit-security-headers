39c1b33529c4fc77fc3945d87d8b5901c26b1622
kevinobee <kevin.obee@gmail.com>
2024-02-11 09:43:02 +0000

    integration tests for missing security headers

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,11 +1,7 @@
-import { HttpResponseHeaders } from '$lib/response.headers.js';
+import { HttpResponseHeaders } from '$lib/headers.js';
 import type { Handle } from '@sveltejs/kit';
 
 HttpResponseHeaders.useOwaspRecommended();
 
-HttpResponseHeaders.useHeaders([
-	{ name: 'X-Sveltekit-Page', value: undefined } // remove a SvelteKit header
-	// { name: 'X-Powered-By', value: 'internet' }  // overwrite a header
-]);
 
 export const handle: Handle = HttpResponseHeaders.applySecurityHeaderHook;
