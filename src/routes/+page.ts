aae83e13ba995bfe5d46539937bff65393433092
kevinobee <kevin.obee@gmail.com>
2024-02-13 16:12:36 +0000

    move README.md link to src/lib/docs/ folder

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,5 +1,5 @@
 export async function load(): Promise<{ content: unknown }> {
-	const md = await import('../docs/README.md');
+	const md = await import('$lib/docs/README.md');
 
 	const content = md.default;
 
