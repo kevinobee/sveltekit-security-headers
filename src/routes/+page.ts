c507475ad273eaf57c5b962f51ff18c65451549b
kevinobee <kevin.obee@gmail.com>
2024-02-13 12:45:55 +0000

    npm run format

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,5 +1,5 @@
-export async function load (): Promise<{ content: unknown; }> {
-	const md = await import( '../docs/README.md' );
+export async function load(): Promise<{ content: unknown }> {
+	const md = await import('../docs/README.md');
 
 	const content = md.default;
 
--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,16 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { mdsvex } from 'mdsvex'
+import { mdsvex } from 'mdsvex';
 
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexOptions = {
-	extensions: ['.md'],
-}
+	extensions: ['.md']
+};
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: [ '.svelte', '.md' ],
-	preprocess: [ vitePreprocess(), mdsvex( mdsvexOptions ) ],
+	extensions: ['.svelte', '.md'],
+	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
