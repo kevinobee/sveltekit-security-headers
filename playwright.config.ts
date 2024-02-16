89572b445eb25cec07ac57b1d6d9c2b65e81e192
kevinobee <kevin.obee@gmail.com>
2024-02-16 14:10:38 +0000

    fix: configure playwright web server

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,10 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
 	webServer: {
 		command: 'npm run build && npm run preview',
-		port: 4173
+		port: 4173,
+		reuseExistingServer: !process.env.CI,
+		stdout: 'ignore',
+		stderr: 'pipe',
 	},
 	testDir: 'tests',
 	reporter: process.env.CI
