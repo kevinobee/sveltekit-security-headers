80d32434aed955bcedc9fe320c5a1ff40af7f639
Kevin Obee <kevin.obee@gmail.com>
2024-02-16 14:15:58 +0000

    Add SEO tests (#42)

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,9 +3,13 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
 	webServer: {
 		command: 'npm run build && npm run preview',
-		port: 4173
+		port: 4173,
+		reuseExistingServer: !process.env.CI,
+		stdout: 'ignore',
+		stderr: 'pipe'
 	},
 	testDir: 'tests',
+	reporter: process.env.CI ? [['github'], ['html', { open: 'never' }]] : 'list',
 	testMatch: /(.+\.)?(test|spec)\.[jt]s/
 };
 
