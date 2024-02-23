cb83ee4ace3218f8dcd71d8f9f7c002654e66aaf
kevinobee <kevin.obee@gmail.com>
2024-02-23 11:26:51 +0000

    add shiki syntax highlighting

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,30 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { mdsvex } from 'mdsvex'
+import { mdsvex, escapeSvelte } from 'mdsvex';
+import { getHighlighter } from 'shiki';
 
+const langs = ['javascript', 'typescript', 'shell'];
+const theme = 'github-dark-dimmed';
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexOptions = {
 	extensions: ['.md'],
-}
+	highlight: {
+		highlighter: async (code, lang = 'text') => {
+			const highlighter = await getHighlighter({
+				themes: [theme],
+				langs: langs
+			});
+			await highlighter.loadLanguage('javascript', 'typescript', 'shell');
+			const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme: theme }));
+			return `{@html \`${html}\` }`;
+		}
+	}
+};
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: [ '.svelte', '.md' ],
-	preprocess: [ vitePreprocess(), mdsvex( mdsvexOptions ) ],
+	extensions: ['.svelte', '.md'],
+	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
