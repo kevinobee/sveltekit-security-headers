235a0052baa592eefdc5318457815bc79edfebc7
kevinobee <kevin.obee@gmail.com>
2024-02-25 16:08:13 +0000

    add remark + rehype plugins

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,9 @@ import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex, escapeSvelte } from 'mdsvex';
 import { getHighlighter } from 'shiki';
+import remarkUnwrapImages from 'remark-unwrap-images';
+import remarkToc from 'remark-toc';
+import rehypeSlug from 'rehype-slug';
 
 const langs = ['http', 'typescript', 'shell'];
 const theme = 'aurora-x';
@@ -19,7 +22,9 @@ const mdsvexOptions = {
 			const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme: theme }));
 			return `{@html \`${html}\` }`;
 		}
-	}
+	},
+	remarkPlugins: [remarkUnwrapImages, [remarkToc, { tight: true }]],
+	rehypePlugins: [rehypeSlug]
 };
 
 /** @type {import('@sveltejs/kit').Config} */
